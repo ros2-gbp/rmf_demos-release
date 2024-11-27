5a4cdab44ef9390acc04d172cf715312dddc0d66
Luca Della Vedova <lucadv@intrinsic.ai>
2024-11-27 16:42:20 +0800

    Rebase from 'upstream'

--- rmf_demos_tasks/rmf_demos_tasks/dispatch_cart_delivery.py
+++ rmf_demos_tasks/rmf_demos_tasks/dispatch_cart_delivery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2021 Open Source Robotics Foundation, Inc.
+# Copyright 2023 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Dispatch a cleaning."""
+"""Dispatch a cart delivery."""
 
 import argparse
 import asyncio
@@ -29,11 +29,10 @@ from rclpy.qos import QoSHistoryPolicy as History
 from rclpy.qos import QoSProfile
 from rclpy.qos import QoSReliabilityPolicy as Reliability
 
-from rmf_task_msgs.msg import ApiRequest
-from rmf_task_msgs.msg import ApiResponse
+from rmf_task_msgs.msg import ApiRequest, ApiResponse
+
 
 ###############################################################################
-
 
 class TaskRequester(Node):
     """Task requester."""
@@ -42,50 +41,24 @@ class TaskRequester(Node):
         """Initialize a task requester."""
         super().__init__('task_requester')
         parser = argparse.ArgumentParser()
-        parser.add_argument(
-            '-cs',
-            '--clean_start',
-            required=True,
-            type=str,
-            help='Cleaning zone',
-        )
-        parser.add_argument(
-            '-F',
-            '--fleet',
-            type=str,
-            help='Fleet name, should define together with robot',
-        )
-        parser.add_argument(
-            '-R',
-            '--robot',
-            type=str,
-            help='Robot name, should define together with fleet',
-        )
-        parser.add_argument(
-            '-st',
-            '--start_time',
-            help='Start time from now in secs, default: 0',
-            type=int,
-            default=0,
-        )
-        parser.add_argument(
-            '-pt',
-            '--priority',
-            help='Priority value for this request',
-            type=int,
-            default=0,
-        )
-        parser.add_argument(
-            '--use_sim_time',
-            action='store_true',
-            help='Use sim time, default: false',
-        )
-        parser.add_argument(
-            '--requester',
-            help='Entity that is requesting this task',
-            type=str,
-            default='rmf_demos_tasks'
-        )
+        parser.add_argument('-p', '--pickup', required=True,
+                            type=str,
+                            help='Pickup waypoint name')
+        parser.add_argument('-d', '--dropoff', required=True,
+                            type=str,
+                            help='Dropoff waypoint name')
+        parser.add_argument('-F', '--fleet', type=str,
+                            help='Fleet name, should define tgt with robot')
+        parser.add_argument('-R', '--robot', type=str,
+                            help='Robot name, should define tgt with fleet')
+        parser.add_argument('-st', '--start_time',
+                            help='Start time from now in secs, default: 0',
+                            type=int, default=0)
+        parser.add_argument('-pt', '--priority',
+                            help='Priority value for this request',
+                            type=int, default=0)
+        parser.add_argument('--use_sim_time', action='store_true',
+                            help='Use sim time, default: false')
 
         self.args = parser.parse_args(argv[1:])
         self.response = asyncio.Future()
@@ -94,12 +67,10 @@ class TaskRequester(Node):
             history=History.KEEP_LAST,
             depth=1,
             reliability=Reliability.RELIABLE,
-            durability=Durability.TRANSIENT_LOCAL,
-        )
+            durability=Durability.TRANSIENT_LOCAL)
 
         self.pub = self.create_publisher(
-            ApiRequest, 'task_api_requests', transient_qos
-        )
+          ApiRequest, 'task_api_requests', transient_qos)
 
         # enable ros sim time
         if self.args.use_sim_time:
@@ -109,7 +80,7 @@ class TaskRequester(Node):
 
         # Construct task
         msg = ApiRequest()
-        msg.request_id = 'clean_' + str(uuid.uuid4())
+        msg.request_id = 'delivery_' + str(uuid.uuid4())
         payload = {}
         if self.args.fleet and self.args.robot:
             self.get_logger().info("Using 'robot_task_request'")
@@ -121,53 +92,54 @@ class TaskRequester(Node):
             payload['type'] = 'dispatch_task_request'
         request = {}
 
-        # Set task request request time and start time
+        # Set task request start time
         now = self.get_clock().now().to_msg()
         now.sec = now.sec + self.args.start_time
-        start_time = now.sec * 1000 + round(now.nanosec / 10**6)
-        request['unix_millis_request_time'] = start_time
+        start_time = now.sec * 1000 + round(now.nanosec/10**6)
         request['unix_millis_earliest_start_time'] = start_time
 
-        request['requester'] = self.args.requester
+        # TODO(luca) expose duration and tool sink to CLI
+        def __create_perform_action(action_category, duration_ms=10000,
+                                    use_tool_sink=False):
+            return {
+                    'unix_millis_action_duration_estimate': duration_ms,
+                    # for internal FleetUpdateHandle to check if action
+                    # is performable by this fleet
+                    'category': action_category,
+                    'description':
+                    {
+                        # for fleet manager to start action process
+                        'deliver_cart_task_name': action_category
+                    },
+                    'use_tool_sink': use_tool_sink
+                    }
 
-        # Define task request category
+        # Define with request category compose
         request['category'] = 'compose'
 
-        if self.args.fleet:
-            request['fleet_name'] = self.args.fleet
-
-        # Define task request description with cleaning zone
+        # Define task request description with phases
         description = {}  # task_description_Compose.json
-        description['category'] = 'clean'
+        description['category'] = 'deliver_cart'
         description['phases'] = []
         activities = []
-
-        # Send robot to start waypoint first
-        activities.append(
-            {'category': 'go_to_place', 'description': self.args.clean_start}
-        )
-        # Send a perform action request for robot to clean area
-        activities.append(
-            {
-                'category': 'perform_action',
-                'description': {
-                    'unix_millis_action_duration_estimate': 60000,
-                    'category': 'clean',
-                    'expected_finish_location': self.args.clean_start,
-                    'description': {'zone': self.args.clean_start},
-                    'use_tool_sink': True,
-                },
-            }
-        )
-
+        # Add each phase
+        activities.append({
+            'category': 'go_to_place',
+            'description': self.args.pickup})
+        activities.append({
+            'category': 'perform_action',
+            'description': __create_perform_action('delivery_pickup')})
+        activities.append({
+            'category': 'go_to_place',
+            'description': self.args.dropoff})
+        activities.append({
+            'category': 'perform_action',
+            'description': __create_perform_action('delivery_dropoff')})
+        # Add activities to phases
         description['phases'].append(
-            {
-                'activity': {
-                    'category': 'sequence',
-                    'description': {'activities': activities},
-                }
-            }
-        )
+            {'activity': {
+                'category': 'sequence',
+                'description': {'activities': activities}}})
 
         request['description'] = description
         payload['request'] = request
@@ -182,6 +154,7 @@ class TaskRequester(Node):
         )
 
         print(f'Json msg payload: \n{json.dumps(payload, indent=2)}')
+
         self.pub.publish(msg)
 
 
@@ -189,14 +162,13 @@ class TaskRequester(Node):
 
 
 def main(argv=sys.argv):
-    """Dispatch a cleaning."""
+    """Dispatch a cart delivery."""
     rclpy.init(args=sys.argv)
     args_without_ros = rclpy.utilities.remove_ros_args(sys.argv)
 
     task_requester = TaskRequester(args_without_ros)
     rclpy.spin_until_future_complete(
-        task_requester, task_requester.response, timeout_sec=5.0
-    )
+        task_requester, task_requester.response, timeout_sec=5.0)
     if task_requester.response.done():
         print(f'Got response: \n{task_requester.response.result()}')
     else:
